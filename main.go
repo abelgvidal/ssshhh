97dee3e4756ab861cf8be2865f9d153caa605d8e
Abel Gonzalez <abel@nextail.co>
2022-09-29 20:38:49 +0200

    wip

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"math/rand"
+	"path/filepath"
+	"encoding/hex"
+	"os"
 )
 
 func index(w http.ResponseWriter, req *http.Request) {
@@ -20,16 +22,48 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func submit(w http.ResponseWriter, req *http.Request) {
-	reqBody, err := ioutil.ReadAll(req.Body)
+	secret := req.FormValue("text")
+	randomFileName := randomFileName()
+
+	f, err := os.Create(filepath.Join("secrets", randomFileName))
 	if err != nil {
-		log.Fatal(err)
-	}
-	io.WriteString(w, fmt.Sprintf("%s", reqBody))
+	    io.WriteString(w, "err 1!\n")
+	    return
+        }
+
+        defer f.Close()
+
+        _, err2 := f.WriteString(fmt.Sprintf("%s", secret))
+	if err2 != nil {
+	    io.WriteString(w, "err 2!\n")
+	    return
+        }
+	
+	io.WriteString(w, fmt.Sprintf("<html><body>Share secret with this ephemeral link (one-use): <a href=\"secret/%s\">%s</a>.</body></html>", randomFileName, randomFileName))
+}
+
+func secretShow(w http.ResponseWriter, req *http.Request) {
+	slug := getField(req, 0)
+	io.WriteString(w, slug)
+}
+
+type ctxKey struct{}
+
+func getField(r *http.Request, index int) string {
+    fields := r.Context().Value(ctxKey{}).([]string)
+    return fields[index]
+}
+
+func randomFileName() string {
+    randBytes := make([]byte, 16)
+    rand.Read(randBytes)
+    return filepath.Join(hex.EncodeToString(randBytes))
 }
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/secret/:slug", secretShow)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
